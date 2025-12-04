e24b891c2b3d630a5618f684ffad60f7125939eb
yiye769895shaoRobertGarrett <marinasirome@mail.ru>
2025-12-04 15:55:37 +0800

    add complex-dist.rs, SepoliaConfig.ts, statistics-customer-local.jsx

---
+++ complex-dist.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\complex-dist.rs
+// Generated at: 2025-10-11T15:13:28.638Z
---
+++ frontend/web/src/statistics-customer-local.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const components-bridge-import = () => { return null; };
